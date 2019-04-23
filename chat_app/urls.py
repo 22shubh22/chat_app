ea6455bff1c743faea47b86ef0f9dfc701cbf9be
Shubham Goyal <22shubh22@gmail.com>
2019-04-23 20:04:28 +0530

    Create Modals and Database

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from chat.views import hello
 
 urlpatterns = [
-    url(r'^admin', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^hello/', hello),
 ]
